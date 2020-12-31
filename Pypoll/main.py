a8040279a9971fdb6c71cb756fe4961b2f1f0fff
@chhpoirier <catherinepoirier@comcast.net>
2020-12-31 15:40:35 -0700

    almost done. need to write to txt file and clean up code

--- Pypoll/main.py
+++ Pypoll/main.py
@@ -1,8 +1,12 @@
 # read csv file three columns voter Id, County and Candidate
-#Calculate 1. The total number of votes cast
-    #2.  a Complete list of Candidate who received votes - From Candidate column
-    #3. The percentage of Votes each candidate won - need to count and divide
-    #4 The Total number of votes each candidate won - need this for #3
+#Calculate 1. The total number of votes cast - count rows in file minus the header
+    #2.  a Complete list of Candidate who received votes - From Candidate column - Create a list with Candidate info. Then create a dictionary with the candidates
+    
+    #4 The Total number of votes each candidate won - need this for #3 - created a counter for each candiate from Dictionary and loop through candiate list and count each time their name shows up
+    #Does this happen within the Dictionary?
+
+    #3. The percentage of Votes each candidate won - need to count and divide - divide each candidates total by the total number of votes cast from #1.
+          
     #5 The winner of the election based on popular vote. 
 import os
 import csv
@@ -10,39 +14,64 @@ import csv
 csvpath=os.path.join('Resources', 'election_data.csv')
 output_path = os.path.join("analysis", "poll_data.txt")
 
-
 csvfile=open(csvpath)
 csvreader=csv.reader(csvfile)
-vote_count=0
 
-#Net_total_dict={} #creating a dictionaly of p&l values
  # Read the header row first
 csv_header = next(csvreader) #removed header from csvreader object but saved it in CSV_header if needed
 print(f"CSV Header: {csv_header}")
 #row_1 = next(csvreader) #removes first row of data and stores it in row_1
 #print(row_1)
-candidate_list=[]
-
-for each_row in csvreader:      #for loop takes a row value and subtracts the value above it to get the change in P & L and appendeds the change_pl value to a list
-           candidate_list.append(each_row[2])  #changes the P&L column to an integer value and adds it to pl_list 
-
-print(candidate_list) 
-#net_total=0
-#hold_row=int(row_1[1]) #row one value for calculation
-#pl_list=[hold_row] #row one value for list
-#change_pl=[]
-#total_change=0
-
-#Calculates the Total number of votes by counting each row after header was removed
-for each_row in csvreader:
-   vote_count=vote_count+1
+candidate_list=[]  #A list of votes cast for the candidates
+candidate_info={}   #A dictionary to determine unique value - Determine who the candiates are
+vote_count=0
+name=[]
+unique_candidate=[]
 
 
+for each_name in csvreader:      #reades each_row from cvsreader column c into candiate_info
+    candidate_list.append(each_name[2])
+
+for each_row in candidate_list:
+     vote_count=vote_count+1
+
+print(f'Vote Count: {vote_count}')
+
+#candidate_info= {"name": "Test Candidate"}
+#print(f'{candidate_info["name"]}')
+#print("------")
+#print(candidate_info)
+for x in candidate_list:
+    if x not in unique_candidate:
+            unique_candidate.append(x)
+
+for name in unique_candidate:
+       candidate_info[name]=0
+
+for y in candidate_list:
+    for z in unique_candidate:
+        if z == y:
+            candidate_info[z]=candidate_info[z]+1
+
+
+print(candidate_info)
+
+highest_count=0
 
 #prints Results
+print(' ')
 print('Election Results')
 print('-----------------------------')
 print(f'Total Votes: {vote_count}')
+for a in candidate_info:
+    print(f' {a} {((candidate_info[a]/vote_count)*100):.3f}%  ({candidate_info[a]})')
+for b in candidate_info:
+    if candidate_info[b] > highest_count:
+        highest_count=candidate_info[b]
+        winner=b
+
+print(f'Winner: {winner}')
+
 print('-----------------------------')
 
 with open(output_path, 'w', newline='') as csvfile:
