28e0f47c571c7329bd6e3c35f9e61fec0f2f28aa
@chhpoirier <catherinepoirier@comcast.net>
2021-01-06 12:11:56 -0700

    all done

--- Pypoll/main.py
+++ Pypoll/main.py
@@ -1,6 +1,7 @@
 # read csv file three columns voter Id, County and Candidate
 #Calculate 1. The total number of votes cast - count rows in file minus the header
-    #2.  a Complete list of Candidate who received votes - From Candidate column - Create a list with Candidate info. Then create a dictionary with the candidates
+    #2.  a Complete list of Candidate who received votes - From Candidate column - Create a list with Candidate info. Find the unique value froms candiate list 
+    #Then create a dictionary with the unique candidates as keys
     
     #4 The Total number of votes each candidate won - need this for #3 - created a counter for each candiate from Dictionary and loop through candiate list and count each time their name shows up
     #Does this happen within the Dictionary?
@@ -19,31 +20,27 @@ csvreader=csv.reader(csvfile)
 
  # Read the header row first
 csv_header = next(csvreader) #removed header from csvreader object but saved it in CSV_header if needed
-#print(f"CSV Header: {csv_header}")
-#row_1 = next(csvreader) #removes first row of data and stores it in row_1
-#print(row_1)
 candidate_list=[]  #A list of votes cast for the candidates
-candidate_info={}   #A dictionary to determine unique value - Determine who the candiates are
-vote_count=0
+candidate_info={}   #A dictionary to with unique candiates as keys and stores total votes
 name=[]
-unique_candidate=[]
+unique_candidate=[] #stores the values of unique candidate from candidate list
 highest_count=0
+vote_count=0
 
 for each_name in csvreader:      #reades each_row from cvsreader column c into candiate_info
     candidate_list.append(each_name[2])
 
 for each_row in candidate_list:  #calculates the votecount
      vote_count=vote_count+1
-#print(f'Vote Count: {vote_count}')
 
-for x in candidate_list:            #gets a unique list of candidates 
+for x in candidate_list:            #gets a unique list of candidates by searching candidate_list and applying unique values to new list
     if x not in unique_candidate:
             unique_candidate.append(x)
 
 for name in unique_candidate:       #assigns key(candidate name) to dictionary and assigns 0 to value
        candidate_info[name]=0       #0 value will be changed to votes cast below
 
-for y in candidate_list:
+for y in candidate_list:            #calculates the totaL votes per candidate by increasing dict count by one each time key name is found in candidate_list
     for z in unique_candidate:
         if z == y:
             candidate_info[z]=candidate_info[z]+1
@@ -54,6 +51,7 @@ print(' ')
 print('Election Results')
 print('-----------------------------')
 print(f'Total Votes: {vote_count}')
+print('-----------------------------')
 for a in candidate_info:
     print(f'{a}: {((candidate_info[a]/vote_count)*100):.3f}%  ({candidate_info[a]})')
 for b in candidate_info:
@@ -70,7 +68,7 @@ with open(output_path, 'w', newline='') as csvfile:
     csvwriter.writerow(['Election Results'])
     csvwriter.writerow(['-----------------------------'])
     csvwriter.writerow([f'Total Votes: {vote_count}'])
-
+    csvwriter.writerow(['-----------------------------'])
     for a in candidate_info:
         csvwriter.writerow([f'{a}: {((candidate_info[a]/vote_count)*100):.3f}%  ({candidate_info[a]})'])
     csvwriter.writerow(['-----------------------------'])
